881968e588f32a048ca43d990a5d33724c51eaa0
ka2ki1 <121303286+ka2ki1@users.noreply.github.com>
2024-05-19 22:56:38 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const text = document.getElementById('text')
+const button = document.getElementById('btn')
+
+button.addEventListener('click',() => {
+  setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました'
+  }, 2000);
+});
